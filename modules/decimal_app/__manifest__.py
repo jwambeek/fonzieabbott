8094c3d4f7dc20fabd3dd2269f10898e58434c44
akshi2501 <74041011+akshi2501@users.noreply.github.com>
2021-01-18 16:08:11 +0530

    decimal (declining factor)

---
+++ modules/decimal_app/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Decimal',
+    'version': '1.5',
+    'Summary': 'Change decimal places',
+    'description': 'To print the new report',
+    'license': 'LGPL-3',
+    'depends': [
+        'account_accountant',
+        'account',
+        'account_consolidation'
+    ],    
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
