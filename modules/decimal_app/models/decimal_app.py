f2c94f66804efa5546e4e7b1327c80fa47f39511
akshi2501 <74041011+akshi2501@users.noreply.github.com>
2021-01-18 16:19:51 +0530

    remove decimal app

--- modules/decimal_app/models/decimal_app.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-
-class Decimal_app(models.Model):
-    _inherit= 'account.asset'
-    method_progress_factor = fields.Float(string='Declining Factor', digits=(12, 4))
-
