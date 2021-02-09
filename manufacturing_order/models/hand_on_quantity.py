113e12bd8816e32eb2ec57e3fba2a68e617d458a
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-02-09 13:31:20 +0800

    Add files via upload

---
+++ manufacturing_order/models/hand_on_quantity.py
@@ -0,0 +1,32 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import AccessError, UserError
+from odoo.tools import float_compare, float_round, float_is_zero, format_datetime
+from odoo.tools.misc import format_date
+
+from odoo.addons.stock.models.stock_move import PROCUREMENT_PRIORITIES
+
+SIZE_BACK_ORDER_NUMERING = 3
+
+
+class MrpProduction(models.Model):
+    
+    _inherit = 'mrp.production'
+
+    inventory_quantity = fields.Float(
+        'Inventoried Quantity', compute='_compute_inventory_quantity',
+        inverse='_set_inventory_quantity', groups='stock.group_stock_manager')
+
+    quantity = fields.Float(
+        'Quantity',
+        help='Quantity of products in this quant, in the default unit of measure of the product',
+        readonly=True)
+    
+
+    
+    @api.depends('quantity')
+    def _compute_inventory_quantity(self):
+        if not self._is_inventory_mode():
+            self.inventory_quantity = 0
+            return
+        for quant in self:
+            quant.inventory_quantity = quant.quantity
