49a132890455d5a0efc2946c96280542a6e00ac1
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-02-10 00:27:06 +0530

    Update hand_on_quantity.py

--- manufacturing_order/models/hand_on_quantity.py
+++ manufacturing_order/models/hand_on_quantity.py
@@ -4,7 +4,7 @@ class StockQuant(models.Model):
     
     _inherit = 'stock.move'
 
-    inventory_quantity = fields.Float(string='Hand On quantity', compute='_compute_inventory_quantity')
+    inventory_quantity = fields.Float(string='On Hand Quantity', compute='_compute_inventory_quantity')
 
     quantity = fields.Float('Quantity', help='Quantity of products in this quant, in the default unit of measure of the product', readonly=True)
     
