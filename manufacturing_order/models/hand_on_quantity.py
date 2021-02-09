c4a93622704b9d369935e5bf542c7011cd70a3fc
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-02-09 18:31:23 +0530

    Update hand_on_quantity.py

--- manufacturing_order/models/hand_on_quantity.py
+++ manufacturing_order/models/hand_on_quantity.py
@@ -8,14 +8,29 @@ from odoo.osv import expression
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 
-#from odoo.addons.stock.models.stock_move import PROCUREMENT_PRIORITIES
+from odoo.addons.stock.models.stock_move import PROCUREMENT_PRIORITIES
 
 
 
 class StockQuant(models.Model):
     
-    _inherit = 'mrp.production'
+    _inherit = 'stock.quant'
 
-    inventory_quantity = fields.Float(string="Hand On Quantity")
+    inventory_quantity = fields.Float(
+        'Inventoried Quantity', compute='_compute_inventory_quantity',
+        inverse='_set_inventory_quantity', groups='stock.group_stock_manager')
 
+    quantity = fields.Float(
+        'Quantity',
+        help='Quantity of products in this quant, in the default unit of measure of the product',
+        readonly=True)
     
+
+
+    @api.depends('quantity')
+    def _compute_inventory_quantity(self):
+        if not self._is_inventory_mode():
+            self.inventory_quantity = 0
+            return
+        for quant in self:
+            quant.inventory_quantity = quant.quantity
