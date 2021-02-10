d0d906539ee41c6a3cef025e0f1547d2e21daeef
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-02-11 01:50:30 +0530

    update

--- excercise_report/models/hand_on_quantity.py
+++ excercise_report/models/hand_on_quantity.py
@@ -4,29 +4,14 @@ from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 
 
-class Transfers(models.Model):
-    _inherit = 'stock.picking'
+class Sale_Order(models.Model):
+    _inherit = 'sale.order'
 
-    def action_assign(self):
-        """ Check availability of picking moves.
-        This has the effect of changing the state and reserve quants on available moves, and may
-        also impact the state of the picking as it is computed based on move's states.
-        @return: True
-        """
-        self.filtered(lambda picking: picking.state == 'draft').action_confirm()
-        
-        moves = self.mapped('move_lines').filtered(lambda move: move.state not in ('draft', 'cancel', 'done'))
-        if not moves:
-            raise UserError(_('Nothing to check the availability for.'))
-        # If a package level is done when confirmed its location can be different than where it will be reserved.
-        # So we remove the move lines created when confirmed to set quantity done to the new reserved ones.
-        package_level_done = self.mapped('package_level_ids').filtered(lambda pl: pl.is_done and pl.state == 'confirmed')
-        package_level_done.write({'is_done': False})
-        moves._action_assign()
-        package_level_done.write({'is_done': True})
+class Product_Template(models.Model):
+    _inherit = 'product.template'
 
-        return True
-        
+
+     
 
 
 
