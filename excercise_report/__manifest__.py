42fa0fe71a57086b89fca761778f3f8dea3419f9
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-02-11 01:36:40 +0530

    update

--- excercise_report/__manifest__.py
+++ excercise_report/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Manufacturing',
-    'version': '2.2',
+    'version': '2.3',
     
     'category': 'Manufacturing/Manufacturing',
     'sequence': 55,
@@ -15,7 +15,7 @@
         #'security/mrp_security.xml',
         #'security/ir.model.access.csv',
         #'views/hand_on_quantity.xml',
-        #'views/product_list_pivot.xml',
+        'views/product_list_pivot.xml',
     ],
    
     
