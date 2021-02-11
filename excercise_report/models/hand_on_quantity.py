4e6b75e1da4f597751e6978363721fa1beb46cda
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-02-11 12:38:10 +0530

    update field

--- excercise_report/models/hand_on_quantity.py
+++ excercise_report/models/hand_on_quantity.py
@@ -2,6 +2,10 @@ from odoo import api, fields, models,_
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
+class Sale_Report(models.Model):
+    _inherit = 'sale.report'
+
+    brand  = fields.Float(related='product_id.product_brand_id', readonly=True)
 
 
 class Transfers(models.Model):
