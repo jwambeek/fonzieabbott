c71441a74e20a0c0c2226502f24739edc2bf647f
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-02-15 16:33:54 +0530

    Update hand_on_quantity.py

--- excercise_report/models/hand_on_quantity.py
+++ excercise_report/models/hand_on_quantity.py
@@ -1,6 +1,6 @@
 from odoo import api, fields, models,_
 from odoo.exceptions import UserError, ValidationError
-#from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+
 
 
 class Sale_Report(models.Model):
