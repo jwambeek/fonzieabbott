4e1cad707e22910a3fe3d094ce0f4ad7d760fef8
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-02-18 11:58:10 +0530

    Update sale_report_class.py

--- modules/decimal_app/models/sale_report_class.py
+++ modules/decimal_app/models/sale_report_class.py
@@ -11,7 +11,7 @@ class Sale_Report_Class(models.Model):
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         with_ = ("WITH %s" % with_clause) if with_clause else ""
 
-        select_ = """
+        select_ = 
             coalesce(min(l.id), -s.id) as id,
             l.product_id as product_id,
             t.uom_id as product_uom,
@@ -49,12 +49,12 @@ class Sale_Report_Class(models.Model):
             t.x_studio_liters_per_unit_eg_03_for_300ml as liters_sold,
             (count(*) * t.x_studio_liters_per_unit_eg_03_for_300ml) as total_liters_sold,
             s.id as order_id
-        """
+        
 
         for field in fields.values():
             select_ += field
 
-        from_ = """
+        from_ = 
                 sale_order_line l
                       right outer join sale_order s on (s.id=l.order_id)
                       join res_partner partner on s.partner_id = partner.id
@@ -64,9 +64,9 @@ class Sale_Report_Class(models.Model):
                     left join uom_uom u2 on (u2.id=t.uom_id)
                     left join product_pricelist pp on (s.pricelist_id = pp.id)
                 %s
-        """ % from_clause
+            % from_clause
 
-        groupby_ = """
+        groupby_ = 
             l.product_id,
             l.order_id,
             t.uom_id,
@@ -91,6 +91,6 @@ class Sale_Report_Class(models.Model):
             partner.commercial_partner_id,
             l.discount,
             s.id %s
-        """ % (groupby)
+            % (groupby)
 
         return '%s (SELECT %s FROM %s GROUP BY %s)' % (with_, select_, from_, groupby_)
