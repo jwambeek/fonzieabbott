451b62d51900113a43e950dec7ff81d047478ac4
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-02-18 08:23:42 +0530

    update

--- modules/decimal_app/__manifest__.py
+++ modules/decimal_app/__manifest__.py
@@ -1,15 +1,22 @@
 {
     'name' : 'Decimal',
-    'version': '1.5',
+    'version': '1.6',
     'Summary': 'Change decimal places',
     'description': 'To print the new report',
     'license': 'LGPL-3',
     'depends': [
         'account_accountant',
         'account',
-        'account_consolidation'
+        'account_consolidation',
+        'stock',
+        'mrp',
+        'sale_management',
+        'product',
     ],    
+    # 'data': [
+    #     'views/mrp_view.xml',
+    # ],
     'installable': True,
     'application':True,
-    'auto_install':False
+    'auto_install':True
 }
--- modules/decimal_app/models/__init__.py
+++ modules/decimal_app/models/__init__.py
@@ -1,1 +1,2 @@
 from . import decimal_app
+from . import sale_report_class
