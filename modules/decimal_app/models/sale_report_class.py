8b712c59382178db379aca9435c0101035092f58
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-02-18 16:23:23 +0530

    Update sale_report_class.py

--- modules/decimal_app/models/sale_report_class.py
+++ modules/decimal_app/models/sale_report_class.py
@@ -46,7 +46,7 @@ class Sale_Report_Class(models.Model):
             CASE WHEN l.product_id IS NOT NULL THEN sum(p.volume * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as volume,
             l.discount as discount,
             CASE WHEN l.product_id IS NOT NULL THEN sum((l.price_unit * l.product_uom_qty * l.discount / 100.0 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END))ELSE 0 END as discount_amount,
-            (t.x_studio_liters_per_unit_eg_03_for_300ml) / count(*) as liters_sold,
+            (t.x_studio_liters_per_unit_eg_03_for_300ml / 2) as liters_sold,
             (count(*) * t.x_studio_liters_per_unit_eg_03_for_300ml) as total_liters_sold,
             s.id as order_id
         """
