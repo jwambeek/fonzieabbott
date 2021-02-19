6ed6cb1a7b243a927b9681ef994316cb64798de7
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-02-19 12:30:18 +0530

    Update __manifest__.py

--- modules/decimal_app/__manifest__.py
+++ modules/decimal_app/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Decimal',
-    'version': '3.7',
+    'version': '4.0',
     'Summary': 'Change decimal places',
     'description': 'To print the new report',
     'license': 'LGPL-3',
