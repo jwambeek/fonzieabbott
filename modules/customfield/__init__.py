ac16d00f149de0b473d7cc9af1dff7ff54d3f28b
jwambeek <jeri@whichaddon.com>
2021-03-11 13:05:03 +0530

    Commit1

---
+++ modules/customfield/__init__.py
@@ -0,0 +1,8 @@
+from . import models
+#from . import controllers
+#from . import report
+#from . import wizard
+
+#from functools import partial
+import odoo
+from odoo import api, SUPERUSER_ID
---
+++ modules/customfield/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Custom',
+    'version': '2.1',
+    'category': 'Sales/Sales',
+    'summary': 'Custom Changes',
+    'description': """Custom Additions""",
+    'depends': ['sales_team', 'payment', 'portal', 'utm'],
+    'data': [
+
+    ],
+    'demo': [
+        'data/product_product_demo.xml',
+        'data/sale_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
