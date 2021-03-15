aab430a252bb38cabfd6c72d0682f8d8994acfe9
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-03-15 12:35:05 +0530

    update query

--- modules/decimal_app/__manifest__.py
+++ modules/decimal_app/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Decimal',
-    'version': '5.5',
+    'version': '5.6',
     'Summary': 'Change decimal places',
     'description': 'To print the new report',
     'license': 'LGPL-3',
